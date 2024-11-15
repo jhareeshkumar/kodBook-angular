f75e8a6aed219debe14b2cf5ddd59c5293bc2b86
jhareeshkumar <jhareesh30@gmail.com>
2024-11-16 03:19:15 +0530

    header componet with styling and routing added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
